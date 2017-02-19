64965585d39779a7b5ccfc080bb78757c5791873
MuchContact <liuhengjiangis@foxmail.com>
2017-02-19 20:55:37 +0800

    add mysql jar and design new demo topology

---
+++ src/main/java/cn/egova/storm_kafka/bolt/cases/TodayCaseFinish.java
@@ -0,0 +1,38 @@
+package cn.egova.storm_kafka.bolt.cases;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+import java.util.Map;
+
+public class TodayCaseFinish extends BaseRichBolt{
+    private static int todayCount = 0;
+
+    private OutputCollector collector;
+
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        this.collector = outputCollector;
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        String action = tuple.getStringByField("action");
+        String type = tuple.getStringByField("type");
+        String content = tuple.getStringByField("content");
+        if("结案".equals(action)) {
+            todayCount ++;
+        }
+        this.collector.emit(new Values(String.format("%s#%d", "今日结案数", todayCount)));
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields("message"));
+    }
+}
--- src/main/java/cn/egova/storm_kafka/topo/KafkaTopo.java
+++ src/main/java/cn/egova/storm_kafka/topo/KafkaTopo.java
@@ -1,11 +1,15 @@
 package cn.egova.storm_kafka.topo;
 
 import backtype.storm.Config;
+import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.spout.SchemeAsMultiScheme;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
 import cn.egova.storm_kafka.bolt.*;
+import cn.egova.storm_kafka.bolt.cases.*;
 import cn.egova.storm_kafka.spout.MessageScheme;
 import storm.kafka.BrokerHosts;
 import storm.kafka.KafkaSpout;
@@ -20,8 +24,13 @@ import java.util.Arrays;
 import java.util.Properties;
 
 /**
+<<<<<<< HEAD
  * storm消费kafka的消息经过计算处理存入到kafka、MySQL中。
  * 常用三种提交topo方式
+=======
+ * storm消费kafka的消息经过计算处理存入到kafka、MySQL中
+ *
+>>>>>>> add mysql jar and design new demo topology
  * @Author tcb
  */
 public class KafkaTopo {
@@ -48,9 +57,11 @@ public class KafkaTopo {
 	private static final Integer STORM_ZOOKEEPER_PORT=2181;
 	private static final Boolean isDebug=true;
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+        runDemo2(args);
+    }
 
-
+    private static void runDemo1(String[] args) {
 		BrokerHosts brokerHosts = new ZkHosts("dev17:2181,dev19:2181");
 		SpoutConfig spoutConfig = new SpoutConfig(brokerHosts, CONSUME_TOPIC, ZK_ROOT, KAFKA_SPOUT_ID);
 		spoutConfig.forceFromStart = false;
@@ -64,7 +75,7 @@ public class KafkaTopo {
 		builder.setBolt(PYTHON_BOLT_IDA,new PythonBoltA(),2).shuffleGrouping(REPORT_BOLT_ID);
 		builder.setBolt(PYTHON_BOLT_IDB,new PythonBoltB(),2).shuffleGrouping(PYTHON_BOLT_IDA);
 		builder.setBolt(MYSQL_BOLT_ID, new MysqlBolt(),4).fieldsGrouping(MSG_CAL_BOLT_ID, new Fields("message", "processTime"));
-     	KafkaBolt bolt = new KafkaBolt();
+		KafkaBolt bolt = new KafkaBolt();
 		bolt.withTopicSelector(new DefaultTopicSelector(PRODUCT_TOPIC))
 				.withTupleToKafkaMapper(new FieldNameBasedTupleToKafkaMapper());
 		builder.setBolt(KAFKA_BOLT_ID, bolt, 1).shuffleGrouping(REPORT_BOLT_ID);
@@ -87,7 +98,13 @@ public class KafkaTopo {
 		conf.setDebug(isDebug);
 		conf.setNumWorkers(3);
 		System.setProperty("storm.jar","E:\\数字政通\\stormTokafka\\storm-example-master\\classes\\artifacts\\storm_example_jar\\storm-example.jar");
-		StormSubmitter.submitTopology(DEFAULT_TOPOLOGY_NAME, conf, builder.createTopology());
+		try {
+			StormSubmitter.submitTopology(DEFAULT_TOPOLOGY_NAME, conf, builder.createTopology());
+		} catch (AlreadyAliveException e) {
+			e.printStackTrace();
+		} catch (InvalidTopologyException e) {
+			e.printStackTrace();
+		}
 	/*	*//**
 		 * 本地提交方式、集群提交方式
 		 *//*
@@ -104,6 +121,57 @@ public class KafkaTopo {
 			new LocalCluster().submitTopology(DEFAULT_TOPOLOGY_NAME, conf, builder.createTopology());
 		}*/
 
-	}
+    }
 
+    private static void runDemo2(String[] args) {
+        BrokerHosts brokerHosts = new ZkHosts("192.168.99.100:2181");
+        SpoutConfig spoutConfig = new SpoutConfig(brokerHosts, "case-event", ZK_ROOT, KAFKA_SPOUT_ID);
+        spoutConfig.forceFromStart = false;
+        spoutConfig.scheme = new SchemeAsMultiScheme(new MessageScheme());
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout(KAFKA_SPOUT_ID, new KafkaSpout(spoutConfig), 3);
+        String caseSplit = "case-split";
+        builder.setBolt(caseSplit, new CaseTypeSplit(), 2).shuffleGrouping(KAFKA_SPOUT_ID);
+        String today_shangbao = "today_shangbao";
+        builder.setBolt(today_shangbao, new TodayCaseReport()).fieldsGrouping(caseSplit, new Fields("type"));
+        String today_lian = "today_lian";
+        builder.setBolt(today_lian, new TodayCaseRegister()).fieldsGrouping(caseSplit, new Fields("type"));
+        String today_jiean = "today_jiean";
+        builder.setBolt(today_jiean, new TodayCaseFinish()).fieldsGrouping(caseSplit, new Fields("type"));
+        String today_gaofa = "today_gaofa";
+        builder.setBolt(today_gaofa, new TodayHotCaseType()).shuffleGrouping(caseSplit);
+        KafkaBolt bolt = new KafkaBolt();
+        String topicName = "case-result";
+        bolt.withTopicSelector(new DefaultTopicSelector(topicName))
+                .withTupleToKafkaMapper(new FieldNameBasedTupleToKafkaMapper());
+        builder.setBolt(KAFKA_BOLT_ID, bolt, 1)
+                .shuffleGrouping(today_shangbao)
+                .shuffleGrouping(today_lian)
+                .shuffleGrouping(today_jiean)
+                .shuffleGrouping(today_gaofa);
+        Config conf = new Config();
+        //3. 设置kafka producer的配置
+        Properties props = new Properties();
+        props.put("metadata.broker.list", "192.168.99.100:32770,192.168.99.100:32768");
+        props.put("producer.type", "async");
+        props.put("request.required.acks", "0"); // 0 ,-1 ,1
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        conf.put(TridentKafkaState.KAFKA_BROKER_PROPERTIES, props);
+        conf.put("topic", topicName);
+        conf.setNumWorkers(3);
+//		conf.setNumAckers(0);
+        conf.setDebug(true);
+        if (args.length > 0) {
+            // cluster submit.
+            try {
+                StormSubmitter.submitTopology(DEFAULT_TOPOLOGY_NAME, conf, builder.createTopology());
+            } catch (AlreadyAliveException e) {
+                e.printStackTrace();
+            } catch (InvalidTopologyException e) {
+                e.printStackTrace();
+            }
+        } else {
+            new LocalCluster().submitTopology(DEFAULT_TOPOLOGY_NAME, conf, builder.createTopology());
+        }
+    }
 }
